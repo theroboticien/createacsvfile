75b3b976b860b03cff840413b74514ce5169863b
theroboticien <theroboticien@gmail.com>
2022-02-20 19:10:13 +0100

    Defining the number of line and coloumn that we need for our files

--- createacsvfile.py
+++ createacsvfile.py
@@ -1,3 +1,15 @@
-#This is a program to create a csv file
-#the aim of this program is to create a csv file to fill different Databases
+# This is a program to create a csv file
+# The aim of this program is to create a csv file to fill different Databases
+# All the data will be strings
+# we can make this better by selecting the type of data entered
+
+# Main program
+# In this part of the program we define how many data we want create
 print('This is a program to create a csv file')
+value = input('please enter the number of coloumn that you want to create: ')
+nbr_coloumn =  int(float(value))
+print(f'The number of coloumn is : {nbr_coloumn}')
+
+value = input('please enter the number of line that you want to create: ')
+nbr_line =  int(float(value))
+print(f'The number of line is : {nbr_line}')
