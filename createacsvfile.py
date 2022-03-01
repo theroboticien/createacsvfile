19847224c1159289a7ff03aa6f42110f3c168c0e
theroboticien <theroboticien@gmail.com>
2022-03-01 21:41:19 +0100

    Adding the documentation of the programm. The documentation is generated automatically using the "pdoc" Module for Python. The HTML file in the html folder is the documentation generated

--- createacsvfile.py
+++ createacsvfile.py
@@ -1,12 +1,16 @@
-# This is a program to create a csv file
-# The aim of this program is to create a csv file to fill different Databases
-# All the data will be strings
+"""
+ This is a program to create a csv file
+ The aim of this program is to create a csv file to fill different Databases
+ All the data will be strings
+"""
+
 from python_random_strings import random_strings
 
 
 # Main program
 # In this part of the program we define how many data we want create
 # How many coloumn to fill
+
 print('This is a program to create a csv file')
 value = input('please enter the number of coloumn that you want to create: ')
 nbr_coloumn =  int(float(value))
@@ -20,17 +24,19 @@ print(f'The number of line is : {nbr_line}')
 # Create a random word
 word = random_strings.random_lowercase(6)
 
-# Open a file with the option to rewrite the file if exist and create file if does not exist
-f = open("data.csv", "w")
+def Fill_CSV(word):
+    """Function to fill the CSV file"""
+    # Open a file with the option to rewrite the file if exist and create file if does not exist
+    f = open("data.csv", "w")
+    # Fill the file with the information needed
+    for i in range(0,nbr_line):
+        for j in range(0,nbr_coloumn):
+            # Create a random word
+            word = random_strings.random_lowercase(6)
+            # Write to the word to the file
+            f.write(f"{word}") if j == nbr_coloumn-1 else f.write(f"{word},")
+            f.write("\n")
+    # Close the opened file
+    f.close()
 
-# Fill the file with the information needed
-for i in range(0,nbr_line):
-    for j in range(0,nbr_coloumn):
-        # Create a random word
-        word = random_strings.random_lowercase(6)
-        # Write to the word to the file
-        f.write(f"{word}") if j == nbr_coloumn-1 else f.write(f"{word},")
-    f.write("\n")
-
-# Close the opened file
-f.close()
+Fill_CSV(word)
